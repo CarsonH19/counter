341f09dd40b027c09d7cc439bba782e7e5b7e2ad
CarsonH19 <questline.carson@gmail.com>
2023-10-09 15:52:06 +0900

    feat: decrease button functioning

---
+++ script.js
@@ -0,0 +1,37 @@
+// 
+// Decrease - lower count
+// Increase - raise count
+// Reset - set count to 0
+// Zero = black
+// Positive = green
+// Negative = red
+
+// Variables
+let count = 0;
+const countNumber = document.querySelector('h2');
+const decreaseBtn = document.querySelector('.decrease');
+const resetBtn = document.querySelector('.reset');
+const increaseBtn = document.querySelector('.increase');
+
+
+// Functions
+function changeCountNumber() {
+  countNumber.textContent = count;
+}
+
+function decreaseCount(){
+  count--;
+  changeCountNumber();
+}
+
+function increaseCount(){}
+
+function resetCount() {}
+
+
+
+
+// Event Listeners
+decreaseBtn.addEventListener('click', decreaseCount);
+resetBtn.addEventListener('click', increaseCount);
+increaseBtn.addEventListener('click', resetCount);
